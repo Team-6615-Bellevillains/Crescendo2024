790053dace4a523371a6e92adf898d8c50714bbe
bhsrobotics6615 <bhsrobotics@vanburenschools.net>
2024-01-28 12:02:32 -0500

    update configuration to match robot

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -20,8 +20,8 @@ import swervelib.parser.PIDFConfig;
 public final class Constants
 {
 
-  public static final double ROBOT_MASS = (148 - 20.3) * 0.453592; // 32lbs * kg per pound
-  public static final Matter CHASSIS    = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
+  public static final double ROBOT_MASS = Units.lbsToKilograms(56); // 32lbs * kg per pound
+  public static final Matter CHASSIS    = new Matter(new Translation3d(0, 0, Units.inchesToMeters(4)), ROBOT_MASS);
   public static final double LOOP_TIME  = 0.13; //s, 20ms + 110ms sprk max velocity lag
 
   public static final class Auton
@@ -44,9 +44,9 @@ public final class Constants
   {
 
     // Joystick Deadband
-    public static final double LEFT_X_DEADBAND  = 0.1;
+    public static final double LEFT_X_DEADBAND  = 0.14;
     public static final double LEFT_Y_DEADBAND  = 0.1;
-    public static final double RIGHT_X_DEADBAND = 0.1;
+    public static final double RIGHT_X_DEADBAND = 0.07;
     public static final double TURN_CONSTANT    = 6;
   }
 }
